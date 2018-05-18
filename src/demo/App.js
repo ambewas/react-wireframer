abb8d07bfc69596174c83ad3b481f2671a027e38
Kevin Decock <kevin.decock@district01.be>
2018-05-18 12:17:43 +0200

    update app

--- src/demo/App.js
+++ src/demo/App.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import Row from "./components/row";
 import Button from "./components/button";
 import ClassComponent from "./components/class";
+import Label from "./components/label";
 
 import createLayouter from "../lib/components/layouter";
 import "./style.css";
@@ -15,7 +16,7 @@ class Main extends Component { // eslint-disable-line
 	constructor() {
 		super();
 		this.state = {
-			hierarchy: [{ "id":"ab6287f0-3294-11e8-a95f-6f6fb5be77f8", "type":"Button", "props":{ "hierarchyPath":"ab6287f0-3294-11e8-a95f-6f6fb5be77f8", "children":"Button" } }, { "id":"acbf0ab0-3294-11e8-a95f-6f6fb5be77f8", "type":"Row", "props":{ "hierarchyPath":"acbf0ab0-3294-11e8-a95f-6f6fb5be77f8", "children":[{ "id":"aeb20110-3294-11e8-a95f-6f6fb5be77f8", "type":"Button", "props":{ "hierarchyPath":"aeb20110-3294-11e8-a95f-6f6fb5be77f8", "children":"Button", "myShape":{ "background":"green" } } }] } }],
+			hierarchy: [{ "id":"firstbutton", "type":"Button", "props":{ "hierarchyPath":"firstbutton", "children":"Button" } }, { "id":"row", "type":"Row", "props":{ "hierarchyPath":"row", "children":[{ "id":"secondbutton", "type":"Button", "props":{ "hierarchyPath":"secondbutton", "children":"Button", "myShape":{ "background":"green" } } }, { "id":"c04110c0-3414-11e8-a6ae-65bfe30c5376", "type":"Button", "props":{ "hierarchyPath":"c04110c0-3414-11e8-a6ae-65bfe30c5376", "children":[] } }, { "id":"c0bde1e0-3414-11e8-a6ae-65bfe30c5376", "type":"Button", "props":{ "hierarchyPath":"c0bde1e0-3414-11e8-a6ae-65bfe30c5376", "children":"Button" } }, { "id":"bf496a50-3414-11e8-a6ae-65bfe30c5376", "type":"Button", "props":{ "hierarchyPath":"bf496a50-3414-11e8-a6ae-65bfe30c5376", "children":"Button" } }, { "id":"bfc09620-3414-11e8-a6ae-65bfe30c5376", "type":"Button", "props":{ "hierarchyPath":"bfc09620-3414-11e8-a6ae-65bfe30c5376", "children":"Button" } }] } }, { "id":"be6dfe70-3414-11e8-a6ae-65bfe30c5376", "type":"Button", "props":{ "hierarchyPath":"be6dfe70-3414-11e8-a6ae-65bfe30c5376", "children":"Button" } }, { "id":"bee97000-3414-11e8-a6ae-65bfe30c5376", "type":"Button", "props":{ "hierarchyPath":"bee97000-3414-11e8-a6ae-65bfe30c5376", "children":"Button" } }],
 		};
 	}
 	render() {
@@ -26,7 +27,7 @@ class Main extends Component { // eslint-disable-line
 					console.log("hierarchy on top", hierarchy);
 					this.setState({ hierarchy });
 				}}
-				components={{ Button, Row, ClassComponent }}
+				components={{ Button, Row, ClassComponent, Label }}
 			/>
 		);
 	}
